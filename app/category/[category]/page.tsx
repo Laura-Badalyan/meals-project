f1e90cc17a91079b96b432a4e32e0cc2bb91b4e1
Laura Badalyan <laura.badalyan@nork.am>
2025-07-25 16:27:29 +0400

    change

--- app/category/[category]/page.tsx
+++ app/category/[category]/page.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import PaginatedMeals from "@/app/components/PaginatedMeals";
 
 type Meal = {
     strMeal: string;
@@ -21,24 +21,11 @@ export default async function CategoryPage({ params }: Params) {
     if (!res.ok) throw new Error(`Failed to fetch ${category}`)
 
     const data = await res.json() as mealResponse;
-    console.log(data)
 
     return (
         <div className=" container mx-auto">
             <h1 className=" w-full justify-between text-center text-5xl font-bold mb-6 text-[#3a6648f5]">Meals in {category}</h1>
-            <ul className="grid md:grid-cols-2 lg:grid-cols-3 gap-20">
-                {
-                    data.meals.map(item => <li
-                        key={item.idMeal}
-                        className=" border-4 border-amber-200 rounded-lg p-6 bg-amber-50"
-                    >
-                        <Link href={`/meal/${item.idMeal}`}>
-                            <img className="" src={item.strMealThumb} alt={item.strMeal} />
-                            <p className="pt-4 font-bold text-xl text-center text-amber-800">{item.strMeal}</p>
-                        </Link>
-                    </li>)
-                }
-            </ul>
+            <PaginatedMeals meals={data.meals} />
         </div>
     )
 }
--- app/components/PaginatedMeals.tsx
+++ app/components/PaginatedMeals.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Link from "next/link";
 import { useMemo, useState } from "react";
 
 type Meal = {
@@ -23,8 +24,31 @@ export default function PaginatedMeals({ meals }: Props) {
         return meals.slice(start, start + itemsPerPage)
     }, [page])
 
-    const prev = () => { setPage(prev => Math.max(1, prev - 1))};
+    const prev = () => { setPage(prev => Math.max(1, prev - 1)) };
 
-    const next = () => { setPage(prev => Math.min(prev + 1, totalPages))};
+    const next = () => { setPage(prev => Math.min(prev + 1, totalPages)) };
 
+
+    return (
+        <div>
+            <ul className="grid md:grid-cols-2 lg:grid-cols-3 gap-20">
+                {
+                    current?.map(item => <li
+                        key={item.idMeal}
+                        className=" border-4 border-amber-200 rounded-lg p-6 bg-amber-50"
+                    >
+                        <Link href={`/meal/${item.idMeal}`}>
+                            <img className="" src={item.strMealThumb} alt={item.strMeal} />
+                            <p className="pt-4 font-bold text-xl text-center text-amber-800">{item.strMeal}</p>
+                        </Link>
+                    </li>)
+                }
+            </ul>
+            <div>
+                <button onClick={prev}>prev</button>
+                <span>{page} / {totalPages}</span>
+                <button onClick={next}>next</button>
+            </div>
+        </div>
+    )
 }
